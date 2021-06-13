0a73033c41475b276ecec5cfcb21ba01b6912e2f
tom <twfarro@wpi.edu>
2021-06-12 20:23:44 -0400

    removed vestigial test cases

--- src/boundary/web.server.spec.js
+++ src/boundary/web.server.spec.js
@@ -6,34 +6,4 @@ describe("App Context Launches", () => {
         const response = await request(await AppConfig.WEB_SERVER()).get("/");
         expect(response.statusCode).toBe(200);
     });
-
-    test("Respond 200 when GET on upload", async() => {
-        const response = await request(await AppConfig.WEB_SERVER()).get("/manage");
-        expect(response.statusCode).toBe(200);
-    });
-
-    test("Respond 200 when GET on known broadcaster", async() => {
-        const response = await request(await AppConfig.WEB_SERVER()).get("/broadcasters/fomtarro");
-        expect(response.statusCode).toBe(200);
-    });
-
-    test("Respond 200 when GET on known broadcaster", async() => {
-        const response = await request(await AppConfig.WEB_SERVER()).get("/broadcasters/who???");
-        expect(response.statusCode).toBe(200);
-    });
-
-    test("Respond 404 when GET on unknown URL", async() => {
-        const response = await request(await AppConfig.WEB_SERVER()).get("/a-fake-url");
-        expect(response.statusCode).toBe(404);
-    });
-
-    test("Respond 403 Denied when POST on Upload with no session", async() => {
-        const response = await request(await AppConfig.WEB_SERVER()).post("/upload");
-        expect(response.statusCode).toBe(403);
-    });
-
-    test("Respond 403 Denied when POST on DELETE with no session", async() => {
-        const response = await request(await AppConfig.WEB_SERVER()).post("/delete");
-        expect(response.statusCode).toBe(403);
-    });
 })
--- src/usecases/populate.html.page.usecase.spec.js
+++ src/usecases/populate.html.page.usecase.spec.js
@@ -4,15 +4,9 @@ const logger = new AppConfig.LOGGER.Logger();
 describe("Tests page building", () => {
     test("Builds home page", async() => {
         const req = {};
-        const page = await AppConfig.POPULATE_HTML_PAGE.populateBroadcasterList(logger, req);
+        const page = await AppConfig.POPULATE_HTML_PAGE.populateHomePage(logger, req, {id:"abcd"});
         const dom = new JSDOM(page);
-        expect(dom.window.document.getElementById('list').firstElementChild.parentNode.children.length).toBeGreaterThanOrEqual(1);
+        expect(dom.window.document.getElementById('alert-title').innerHTML).toEqual('404');
     });
 
-    test("Builds list page", async() => {
-        const req = {};
-        const page = await AppConfig.POPULATE_HTML_PAGE.populateFileList(logger, req, 'bonbombs');
-        const dom = new JSDOM(page);
-        expect(dom.window.document.getElementById('list').firstElementChild.parentNode.children.length).toBeGreaterThanOrEqual(1);
-    });
 });
